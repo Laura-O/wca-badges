7d1ca5a92837805b8500b2a4a4d182b06808c98f
Laura Ohrndorf <laura.ohrndorf@googlemail.com>
2022-08-11 14:33:17 +0000

    Add registration list and options

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-from calendar import weekday
 import country_converter as coco
-import datetime
 import dateutil.parser
 import json
 import pdfkit
 from PIL import Image
 import os
-from jinja2 import Environment, PackageLoader, select_autoescape, FileSystemLoader
+from jinja2 import Environment, select_autoescape, FileSystemLoader
 import streamlit as st
 import streamlit.components.v1 as components
 import base64
+from operator import itemgetter
 
 env = Environment(loader=FileSystemLoader("."), autoescape=select_autoescape())
 template = env.get_template("./templates/template.html")
-badge = env.get_template("./templates/badge.html")
+badge_template = env.get_template("./templates/badge.html")
+registration_list_template = env.get_template("./templates/registration_list.html")
 logo_path = "static/images/wca.png"
 
 options = {
@@ -31,7 +31,7 @@ options = {
 }
 
 
-st.set_page_config(layout="centered", page_icon="",
+st.set_page_config(layout="wide", page_icon="🔖",
                    page_title="Badge Generator")
 st.title("WCA Competition Badge Generator")
 
@@ -84,84 +84,129 @@ def create_personal_schedule(assignments, events):
 
     return sorted_schedule
 
-
-with st.form("template_form"):
-    left, right = st.columns(2)
-    logo = left.file_uploader(
-        "Upload Logo", type=['png', 'jpg', 'jpeg', 'svg'])
-    wcif = right.file_uploader("Upload JSON", type=['json'])
-
-    if logo is not None:
-        file_details = {"filename": logo.name,
-                        "filetype": logo.type, "filesize": logo.size}
-        st.write(file_details)
-        st.image(load_image(logo), width=150)
-
-        logo_path = "static/images/" + logo.name
-
-        with open(os.path.join(logo_path), "wb") as f:
-            f.write((logo).getbuffer())
-
-    if wcif is not None:
-        file_details = {"filename": wcif.name,
-                        "filetype": wcif.type, "filesize": wcif.size}
-        st.write(file_details)
-
-        with open(os.path.join("tmp/wcif.json"), "wb") as f:
-            f.write((wcif).getbuffer())
-
-    submit = st.form_submit_button()
-
-
-if submit:
-    with open('tmp/wcif.json') as f:
-        data = json.load(f)
-
-    persons = data['persons']
-    schedule = data['schedule']
-
-    events = generate_events_dict(schedule)
-
+def generate_badges(persons, encoded_logo):
     badges = ""
-
-    encoded = base64.b64encode(
-        open(logo_path, 'rb').read()).decode('utf-8')
-
     for p in persons:
-        if (p['registration']['status'] == 'accepted'):
+        if (p['registration']['status'] == 'accepted') and p['registrantId'] < 20:
             person_schedule = create_personal_schedule(
                 p['assignments'], events)
 
-            person_badge = badge.render(
+            person_badge = badge_template.render(
                 name=p['name'],
                 competitor_id=p['registrantId'],
                 wca_id=p['wcaId'],
                 country=coco.convert(names=p['countryIso2'], to='name_short'),
                 schedule=person_schedule,
-                logo=encoded
+                logo=encoded_logo
             )
 
             badges += person_badge
+            
+            return badges
 
-    res = template.render(badges=badges)
+def generate_registration_list(persons):
+    registration_list_data = {}
+    
+    for p in persons:
+        if (p['registration']['status'] == 'accepted') and p['registrantId'] < 20:
+            registration_list_data[p['registrantId']] = {
+                'name': p['name'],
+                'wcaId': p['wcaId'],
+                'birthdate': p['birthdate'] if p['birthdate'] else ''
+            }
+            
+    registration_list = registration_list_template.render(persons = persons)
 
-    st.success("🎉 Your badges were generated!")
+    return registration_list
+            
+        
+  
+left_upload, right_upload = st.columns(2)
+logo = left_upload.file_uploader("Upload Logo", type=['png', 'jpg', 'jpeg', 'svg'])
+wcif = right_upload.file_uploader("Upload JSON", type=['json'])
 
-    with open("static/badges.html", "w") as file:
-        file.write(res)
+if logo is not None:
+    file_details = {"filename": logo.name,
+                        "filetype": logo.type, "filesize": logo.size}
+    
+    left_upload.write(file_details)
+    left_upload.image(load_image(logo), width=150)
 
-    with open("static/badges.html", "r") as file:
-        btn = st.download_button(
-            "⬇️ Download HTML",
-            data=file,
-            file_name="badges.html"
+    logo_path = "static/images/" + logo.name
+
+    with open(os.path.join(logo_path), "wb") as f:
+            f.write((logo).getbuffer())
+
+if wcif is not None:
+    file_details = {"filename": wcif.name,
+                        "filetype": wcif.type, "filesize": wcif.size}
+    right_upload.write(file_details)
+
+    with open(os.path.join("tmp/wcif.json"), "wb") as f:
+            f.write((wcif).getbuffer())
+
+with st.form("template_form"):
+    checkbox_badges = st.checkbox('Badges', value=True)
+    checkbox_registration_list = st.checkbox('Registration list')
+    submit = st.form_submit_button("Generate badges!")
+
+if submit:
+    with open('tmp/wcif.json') as f:
+        data = json.load(f)
+
+    persons = sorted(data['persons'], key=itemgetter('name'))
+    schedule = data['schedule']
+
+    events = generate_events_dict(schedule)
+
+    encoded_logo = base64.b64encode(
+        open(logo_path, 'rb').read()).decode('utf-8')    
+
+    if checkbox_badges:
+        with st.spinner('Generating badges...'):
+            badges = generate_badges(persons, encoded_logo)
+            res = template.render(badges=badges)
+            with open("static/badges.html", "w") as file:
+                file.write(res)
+                
+        st.success("🎉 Your badges were generated!")
+        pdf_badges = pdfkit.from_string(res, False, options=options)
+        
+        with open("static/badges.html", "r") as file:
+            btn = st.download_button(
+                "⬇️ Download badges as HTML",
+                data=file,
+                file_name="badges.html"
+            )
+            
+        st.download_button(
+            "⬇️ Download badges as PDF",
+            data=pdf_badges,
+            file_name="badges.pdf",
+            mime="application/octet-stream",
         )
-
-    pdf = pdfkit.from_string(res, False, options=options)
-
-    st.download_button(
-        "⬇️ Download PDF",
-        data=pdf,
-        file_name="badges.pdf",
-        mime="application/octet-stream",
-    )
+    
+    
+    
+    
+    if checkbox_registration_list:            
+        registration_list = generate_registration_list(persons)
+        with open("static/registration_list.html", "w") as file:
+            file.write(registration_list)
+        
+        st.success("🎉 Your registration list was generated!")
+        pdf_registration_list = pdfkit.from_string(registration_list, False, options=options)
+        
+        with open("static/registration_list.html", "r") as file:
+            btn = st.download_button(
+                "⬇️ Download registration list as HTML",
+                data=file,
+                file_name="registration_list.html"
+            )
+            
+        st.download_button(
+            "⬇️ Download registration list as PDF",
+            data=pdf_registration_list,
+            file_name="registration_list.pdf",
+            mime="application/octet-stream",
+        )
